f43601a0920ee3a863e0396630912fd8556855b6
Mostafa Yasin <mostafa.ys2014@gmail.com>
2019-06-09 06:56:58 +0200

    Re-Designed Navbar to fit to all pages, attached navbar to communities page.

--- js/communities.js
+++ js/communities.js
@@ -1,3 +1,16 @@
-$(document).on('ready', function () {
+$(function () {
+    
+    var navItems = $('.nav-item');
+
+    console.log(navItems.length);
+
+    for (let i = 0; i < navItems.length; i++) {
+        var el = navItems[i];
+        el = $(el);
+
+        el.on('click', function () {
+            $(this).addClass('active').siblings().removeClass('active');
+        });
+    }
 
 });
