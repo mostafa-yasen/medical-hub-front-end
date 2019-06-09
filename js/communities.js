578d920a4efa9fa07d0a2bf226b3cb089e5b03a1
Mostafa Yasin <mostafa.ys2014@gmail.com>
2019-06-09 08:41:31 +0200

    Attached nice-scroll lib to communities page.

--- js/communities.js
+++ js/communities.js
@@ -1,8 +1,15 @@
 $(function () {
-    
-    var navItems = $('.nav-item');
 
-    console.log(navItems.length);
+    $("body").niceScroll({
+        cursorcolor: "#AC42B6",
+        cursoropacitymin: .5,
+        cursoropacitymax: .9,
+        cursorwidth: "5px", 
+        cursorborder: "none",
+        smoothscroll: false
+    });
+
+    var navItems = $('.nav-item');
 
     for (let i = 0; i < navItems.length; i++) {
         var el = navItems[i];
