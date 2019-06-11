b250ddca38ffe4d68a6ce3b141994a2bef5847d9
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2019-06-11 10:42:27 +0200

    Checkpoint.

--- js/communities.js
+++ js/communities.js
@@ -1,23 +1,23 @@
 $(function () {
 
-    $("body").niceScroll({
-        cursorcolor: "#AC42B6",
-        cursoropacitymin: .5,
-        cursoropacitymax: .9,
-        cursorwidth: "5px", 
-        cursorborder: "none",
-        smoothscroll: false
-    });
+    // $("body").niceScroll({
+    //     cursorcolor: "#AC42B6",
+    //     cursoropacitymin: .5,
+    //     cursoropacitymax: .9,
+    //     cursorwidth: "5px", 
+    //     cursorborder: "none",
+    //     smoothscroll: false
+    // });
 
-    var navItems = $('.nav-item');
+    // var navItems = $('.nav-item');
 
-    for (let i = 0; i < navItems.length; i++) {
-        var el = navItems[i];
-        el = $(el);
+    // for (let i = 0; i < navItems.length; i++) {
+    //     var el = navItems[i];
+    //     el = $(el);
 
-        el.on('click', function () {
-            $(this).addClass('active').siblings().removeClass('active');
-        });
-    }
+    //     el.on('click', function () {
+    //         $(this).addClass('active').siblings().removeClass('active');
+    //     });
+    // }
 
 });
